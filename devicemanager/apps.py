b386fd4b1c1fc0d6a366c273770988370baf514a
root <mateusbp.up@hotmail.com>
2016-12-12 12:18:05 -0300

    apps.py added

---
+++ devicemanager/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+""" Device manager app config. """
+from django.apps import AppConfig
+
+
+class DeviceManagerConfig(AppConfig):
+    """ Device manager app config class. """
+    name = 'devicemanager'
+    verbose_name = 'Device Manager'
