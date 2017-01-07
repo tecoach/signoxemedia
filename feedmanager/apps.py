588a2205f31bac2f6dafc434cefc967c5fc5cbb4
root <mateusbp.up@hotmail.com>
2017-01-07 15:37:17 -0300

    apps.py updated

--- feedmanager/apps.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Feed manager app config. """
-from django.apps import AppConfig
-
-
-class FeedManagerConfig(AppConfig):
-    """ Feed manager app config class. """
-    name = 'feedmanager'
-    verbose_name = 'Feed Manager'
