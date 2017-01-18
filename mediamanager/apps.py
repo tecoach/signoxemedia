7040e5d68bf8e3cc7a903bcd668242e537752584
root <mateusbp.up@hotmail.com>
2017-01-18 13:35:34 -0300

    apps.py added

---
+++ mediamanager/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+""" Media manager app config. """
+from django.apps import AppConfig
+
+
+class MediaManagerConfig(AppConfig):
+    """ Media manager app config class. """
+    name = 'mediamanager'
+    verbose_name = 'Media Content Manager'
