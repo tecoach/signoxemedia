ff9287f5e57726a5ac1a8a4cbd7384488aec4fd9
root <mateusbp.up@hotmail.com>
2017-03-20 12:47:03 -0300

    devices.py updated

--- signoxe_server/urls/devices.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-""" URL routing configuration for the devices sub-domain. """
-from django.conf.urls import include, url
-
-import devicemanager.urls
-
-urlpatterns = [
-    url(r'', include(devicemanager.urls)),
-]
