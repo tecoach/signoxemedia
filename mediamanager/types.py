765f92152b9eb674ba45127f2d0c86e451393064
root <mateusbp.up@hotmail.com>
2017-06-13 11:01:45 -0300

    types.py added

---
+++ mediamanager/types.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+""" This module defines common types that can be used across the app. """
+
+
+class AssetTypes(object):
+    """ This class exists simply to contain contants related to the different asset types. """
+    VIDEO = 'VIDEO'
+    IMAGE = 'IMAGE'
+    WEB = 'WEB'
+    FEED = 'FEED'
+    CALENDAR = 'CALENDAR'
