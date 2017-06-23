08dd688c149e0bd6cdffff76f259da0a7d3956f2
root <mateusbp.up@hotmail.com>
2017-06-23 18:36:27 -0300

    asgi.py updated

--- signoxe_server/asgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-from channels.asgi import get_channel_layer
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signoxe_server.settings')
-
-channel_layer = get_channel_layer()
