f087b512f8db85cea3efbf7065f31f6f3bfd61eb
root <mateusbp.up@hotmail.com>
2017-08-13 11:57:54 -0300

    apps.py added

---
+++ client_manager/apps.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+""" Client manager app config. """
+
+from django.apps import AppConfig
+
+
+class ClientManagerConfig(AppConfig):
+    """ Client manager app config class. """
+    name = 'client_manager'
+    verbose_name = 'Client Manager'
