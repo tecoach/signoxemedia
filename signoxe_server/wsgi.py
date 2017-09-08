b2ac77d7c7c19d054e1a06f38abb1b17b19b6116
root <mateusbp.up@hotmail.com>
2017-09-08 18:05:53 -0300

    wsgi.py updated

--- signoxe_server/wsgi.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-""" WSGI module for Signoxe server. """
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signoxe_server.settings')
-
-application = get_wsgi_application()
