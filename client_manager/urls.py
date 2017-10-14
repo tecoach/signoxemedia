9c8c9312c27b2668c6622c0b85004098be9c24bf
root <mateusbp.up@hotmail.com>
2017-10-14 10:47:35 -0300

    urls.py updated

--- client_manager/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-""" URL routing configuration for client manager app. """
-from django.conf.urls import url
-
-from .views import login_as_user
-
-urlpatterns = [
-    url(r'login-as/(?P<user_id>\d+)/', login_as_user, name='login-as-view'),
-]
